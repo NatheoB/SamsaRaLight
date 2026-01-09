770e785a1876994b872ecccb1cfb68be0e8847e2
BEAUCHAMP Natheo <beauchamp.natheo@gmail.com>
2026-01-09 16:13:49 +0100

    delete dev folder

--- dev/create_pkgdown.R
+++
@@ -1,8 +0,0 @@
-# Install released version from CRAN
-install.packages("pkgdown")
-
-# Run once to configure your package to use pkgdown
-# usethis::use_pkgdown()
-
-# Build website
-pkgdown::build_site()
